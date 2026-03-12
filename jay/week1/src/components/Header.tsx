10580db8e6e8552ff3935ec1e35c340fe0eee3a3
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-03-12 21:01:16 +0900

    style: 헤더 컴포넌트 CSS 수정

--- jay/week1/src/components/Header.tsx
+++ jay/week1/src/components/Header.tsx
@@ -7,8 +7,8 @@ interface HeaderProps {
 
 export default function Header({ title, description, showBackButton, badgeLabel }: HeaderProps) {
     return (
-        <header className="flex bg-white w-full h-55 items-center px-6 shadow-sm">
-            <div className="flex flex-col justify-center w-full max-w-4xl mx-auto p-4">
+        <header className="flex bg-white w-full py-7 items-center px-6 shadow-xs border-b border-gray-100">
+            <div className="flex flex-col justify-center w-full max-w-6xl mx-auto p-4">
                 {showBackButton && <button>← 돌아가기</button>}
                 {badgeLabel && (
                     <div className="flex items-center gap-2 mb-3">
@@ -20,9 +20,9 @@ export default function Header({ title, description, showBackButton, badgeLabel 
                         </span>
                     </div>
                 )}
-                <h1 className="text-3xl font-bold mb-2">{title}</h1>
+                <h1 className="text-3xl font-extrabold mb-4 text-slate-900 tracking-tight">{title}</h1>
                 {/* whitespace-pre-line 클래스: \n 줄바꿈 인식*/}
-                {description && <p className="pl-0.25 font-medium text-gray-500 whitespace-pre-line">{description}</p>}
+                {description && <p className="font-medium text-slate-500 whitespace-pre-line leading-relaxed text-base">{description}</p>}
             </div>
         </header>
     );
