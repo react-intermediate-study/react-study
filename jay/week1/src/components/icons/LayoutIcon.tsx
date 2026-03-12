ec8986ccbbf69da4047d2ad4ccc36037f21d4414
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-03-12 22:35:43 +0900

    design: PatternCard 컴포넌트에 사용될 아이콘 컴포넌트 추가

---
+++ jay/week1/src/components/icons/LayoutIcon.tsx
@@ -0,0 +1,21 @@
+export default function LayoutIcon() {
+    return (
+        <div className="flex h-12 w-12 items-center justify-center rounded-2xl text-xl bg-purple-50 text-purple-500">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                <line x1="3" y1="9" x2="21" y2="9" />
+                <line x1="9" y1="21" x2="9" y2="9" />
+            </svg>
+        </div>
+    );
+}
