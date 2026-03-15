07fe7788592161326c43b15b9e633f3ebf0ed043
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-03-15 13:14:16 +0900

    각 상세 페이지에서 헤더에 사용할 패턴 데이터 작성

---
+++ jay/week1/src/constants/PatternData.ts
@@ -0,0 +1,17 @@
+export const PatternData = [
+    {
+        id: 1,
+        title: "SRP (단일 책임 원칙)",
+        description: "Single Responsibility Principle - 하나의 컴포넌트 하나의 역할만 수행해야 합니다.",
+    },
+    {
+        id: 2,
+        title: "Container 패턴",
+        description: "로직(Container/Hook)과 UI(Presentational)를 분리하여 각각의 역할을 명확히 합니다.",
+    },
+    {
+        id: 3,
+        title: "Compound Component",
+        description: "Context를 활용해 부모-자식 간 암묵적 상태 공유로 유연한 컴포넌트를 만듭니다. ",
+    }
+];
