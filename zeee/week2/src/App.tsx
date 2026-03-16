ce7e212bbe5e17a75a9f9c4c5e299ecfb348eb8a
yoonyoungyang <youngonly0118@gmail.com>
2026-03-16 15:51:50 +0900

    step2. 로그인 실패 경우의 수 나누고 에러 메세지 내보내기

--- zeee/week2/src/App.tsx
+++ zeee/week2/src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
+const [passwordError, setPasswordError] = useState("")
+
   return (
     <main>
-      <form>
+      <form noValidate onSubmit={
+        (e)=>{e.preventDefault() 
+                setEmailError("")
+        setPasswordError("")
+        let hasError = false
+        console.log(email, password)
+
+        if (!email.includes("@")) {
+          setEmailError("이메일이 형식에 맞지 않습니다")
+          hasError = true
+        }
+        if (password.length < 8) {
+setPasswordError("비밀번호가 형식에 맞지 않습니다.")
+hasError = true
+        }
+        if (hasError) return
+
+
+        }}>
         <h1>로그인 화면</h1>
         <div>
           <label htmlFor="email">이메일 입력</label>
@@ -9,15 +34,22 @@ function App() {
             id="email"
             type="email"
             placeholder="이메일을 입력해주세요."
-          ></input>
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+         />
+         {emailError && <p>{emailError}</p>}
         </div>
         <div>
           <label htmlFor="password">비밀번호 입력</label>
           <input
             id="password"
             type="password"
+            value={password}
             placeholder="비밀번호를 입력해주세요. "
-          ></input>
+            onChange={(e)=> setPassword(e.target.value)}
+         />
+                  {passwordError && <p>{passwordError}</p>}
+
         </div>
 
         <button type="submit">제출하기</button>
