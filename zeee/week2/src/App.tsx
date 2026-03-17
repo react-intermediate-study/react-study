4e1db16ce87c25dfa2e499b266558dd73a9a6f33
yoonyoungyang <youngonly0118@gmail.com>
2026-03-18 01:17:54 +0900

    step6. useReducer 구현 완료

--- zeee/week2/src/App.tsx
+++ zeee/week2/src/App.tsx
@@ -1,44 +1,104 @@
-import { useState } from "react"
+import { useReducer } from "react"
+
+
+type State = {
+email: string
+  password: string
+  emailError: string
+  passwordError: string
+  loginFail: string
+  loginSuccess: string
+}
+
+type Action =
+  | { type: "CHANGE_EMAIL"; payload: string }
+  | { type: "CHANGE_PASSWORD"; payload: string }
+  | { type: "SET_EMAIL_ERROR"; }
+  | { type: "SET_PASSWORD_ERROR"; }
+   | { type: "SET_LOGIN_FAIL"; }
+  | { type: "SET_LOGIN_SUCCESS"; }
+  | { type: "RESET_MESSAGES"; }
+  | { type: "RESET_FORM"; }
+  | { type: "CLEAR_EMAIL_ERROR" }
+| { type: "CLEAR_PASSWORD_ERROR" }
+
+
+
+
+ function reducer(state:State, action:Action):State {
+  switch( action.type) {
+    case "CHANGE_EMAIL":
+      return{...state, email:action.payload,}
+    case "CHANGE_PASSWORD" :
+      return {
+        ...state, password: action.payload,
+      }
+      case "SET_EMAIL_ERROR" :
+        return{...state, emailError:"이메일이 형식에 맞지 않습니다."}
+            case "SET_PASSWORD_ERROR" :
+        return{...state, passwordError:"비밀번호가 형식에 맞지 않습니다."}
+            case "SET_LOGIN_FAIL" :
+        return{...state, loginFail:"이메일 또는 비밀번호가 틀렸습니다."}
+            case "SET_LOGIN_SUCCESS" :
+        return{...state, loginSuccess:"로그인에 성공했습니다."}
+            case "RESET_MESSAGES" :
+        return{...state, emailError:"", passwordError:"", loginFail:"", loginSuccess:""}
+            case "RESET_FORM" :
+        return{...state, email:"", password:""}
+            case "CLEAR_EMAIL_ERROR":
+        return {...state, emailError:""}
+        case "CLEAR_PASSWORD_ERROR":
+          return {...state, passwordError:""}
+
+      default: return state
+  }
+  
+}
+
+const initialState:State = {
+  email: "",
+  password: "",
+  emailError: "",
+  passwordError: "",
+  loginFail: "",
+  loginSuccess: ""
+}
+
 
 function App() {
   const correctEmail = "test@example.com"
 const correctPassword = "abc1234!"
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [emailError, setEmailError] = useState("")
-const [passwordError, setPasswordError] = useState("")
-const [loginFail, setLoginFail] = useState("")
-const [loginSuccess, setLoginSuccess] = useState("")
 const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+~`\-={}[\]:;"'<>,.?/\\]).{8,}$/
+
+const [state, dispatch] = useReducer(reducer, initialState)
+
+
 
   return (
     <main>
       <form noValidate onSubmit={
-        (e)=>{e.preventDefault() 
-                setEmailError("")
-        setPasswordError("")
-        setLoginFail("")
-        setLoginSuccess("")
+        (e)=>{e.preventDefault()
+dispatch({type:"RESET_MESSAGES"})
         let hasError = false
-        console.log(email, password)
+        console.log(state.email, state.password)
 
-        if (!email.includes("@")) {
-          setEmailError("이메일이 형식에 맞지 않습니다")
-          hasError = true
+        if (!state.email.includes("@")) {
+dispatch({type:"SET_EMAIL_ERROR"})
+hasError = true
         }
-        if (!passwordRegex.test(password)) {
-setPasswordError("비밀번호가 형식에 맞지 않습니다.")
-
+        if (!passwordRegex.test(state.password)) {
+dispatch({type:"SET_PASSWORD_ERROR"})
 hasError = true
         }
         if (hasError) return 
-        if (email !== correctEmail || password !== correctPassword) {
-          setLoginFail("이메일 또는 비밀번호가 올바르지 않습니다.")
-          return
+        if (state.email !== correctEmail || state.password !== correctPassword) {
+        dispatch({type:"SET_LOGIN_FAIL"})
+        alert("이메일 또는 비밀번호가 올바르지 않습니다.")
+        return
         }
         
-          setLoginSuccess("로그인에 성공했습니다.")
-          console.log("로그인 성공")
+          dispatch({type: "SET_LOGIN_SUCCESS"})
+          alert("로그인에 성공했습니다.")
 
         }}>
         <h1>로그인 화면</h1>
@@ -48,27 +108,52 @@ hasError = true
             id="email"
             type="email"
             placeholder="이메일을 입력해주세요."
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={state.email}
+            onChange={(e) => {
+                const value = e.target.value
+
+  dispatch({
+    type: "CHANGE_EMAIL",
+    payload: value,
+  })
+        if (!state.email.includes("@")) {
+dispatch({type:"SET_EMAIL_ERROR"})
+}              
+
+else { dispatch({type:"CLEAR_EMAIL_ERROR"})
+}}}
+            
          />
-         {emailError && <p>{emailError}</p>}
+         {state.emailError && <p>{state.emailError}</p>}
         </div>
         <div>
           <label htmlFor="password">비밀번호 입력</label>
           <input
             id="password"
             type="password"
-            value={password}
+            value={state.password}
             placeholder="비밀번호를 입력해주세요. "
-            onChange={(e)=> setPassword(e.target.value)}
-         />
-                  {passwordError && <p>{passwordError}</p>}
+onChange={(e) => {
+  const value = e.target.value
+
+  dispatch({
+    type: "CHANGE_PASSWORD",
+    payload: value,
+  })
+
+  if (!passwordRegex.test(value)) {
+    dispatch({ type: "SET_PASSWORD_ERROR" })
+  } else {
+    dispatch({ type: "CLEAR_PASSWORD_ERROR" })
+  }
+}}         />
+                  {state.passwordError && <p>{state.passwordError}</p>}
 
         </div>
 
         <button type="submit">제출하기</button>
-              {loginFail && <p>{loginFail}</p>}
-              {loginSuccess && <p>{loginSuccess}</p>}
+              {state.loginFail && <p>{state.loginFail}</p>}
+              {state.loginSuccess && <p>{state.loginSuccess}</p>}
 
       </form>
     </main>
